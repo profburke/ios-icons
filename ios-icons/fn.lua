355e8d65d7b957719bf8bc7017b9497583547611
Matthew M. Burke <matthew@bluedino.net>
2016-03-17 22:18:59 -0400

    fixed fn.select to properly iterate over all types of items

--- ios-icons/fn.lua
+++ ios-icons/fn.lua
@@ -1,15 +1,29 @@
+local insert = table.insert
 local fn = {}
 
 
--- cond should be a predicate
+local append = function(a, b)
+   for _, v in ipairs(b) do
+      insert(a, v)
+   end
+end
+
+
 fn.select = function(tab, cond)
-   assert(type(cond) == 'function', 'cond must be a predicate function')
-   local insert = table.insert
+   assert(type(cond) == 'function', 'cond must be a predicate')
    local result = {}
 
    for _,v in pairs(tab) do
-      if cond(v) then
-         insert(result, v)
+      if type(v) == 'folder' then
+         local fresult = fn.select(v.icons, cond)
+         append(result, fresult)
+      elseif type(v) == 'page' then
+         local fresult = fn.select(v, cond)
+         append(result, fresult)
+      elseif type(v) == 'icon' then
+         if cond(v) then
+            insert(result, v)
+         end
       end
    end
 
